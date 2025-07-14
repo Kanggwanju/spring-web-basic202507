ab212d8926b5837f21cad3084798bd0f8eb1ee6d
Kanggwanju <xprtktm1@gmail.com>
2025-07-14 09:49:15 +0900

    스프링 MVC 컨트롤러 경로변수와 쿼리 파라미터 연습

--- src/main/java/com/spring/basic/chap2_3/controller/ProductController.java
+++ src/main/java/com/spring/basic/chap2_3/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.spring.basic.chap2_3.controller;
 
 import com.spring.basic.chap2_3.entity.Product;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -51,5 +48,9 @@ public class ProductController {
         return productMap.get(id);
     }
 
-
+    // localhost:9000/products/1    -> 1번 상품 조회
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable("id") long id) {
+        return productMap.get(id);
+    }
 }
