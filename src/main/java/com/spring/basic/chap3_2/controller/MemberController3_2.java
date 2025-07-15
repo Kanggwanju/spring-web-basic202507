c41052fe8f0a1901351a337c5aecaf1a4e282cd4
Kanggwanju <xprtktm1@gmail.com>
2025-07-15 11:13:04 +0900

    RequestBody 학습

--- src/main/java/com/spring/basic/chap3_2/controller/MemberController3_2.java
+++ src/main/java/com/spring/basic/chap3_2/controller/MemberController3_2.java
@@ -1,11 +1,9 @@
 package com.spring.basic.chap3_2.controller;
 
 import com.spring.basic.chap3_2.entity.Member;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v3-2/members")
@@ -34,7 +32,45 @@ public class MemberController3_2 {
 
         memberStore.put(member1.getUid(), member1);
         memberStore.put(member2.getUid(), member2);
-
-
     }
+
+    // 전체 조회
+    @GetMapping
+    public List<Member> list() {
+        return new ArrayList<>(memberStore.values());
+    }
+
+    // 회원 등록
+    // ?account=xxx&password=xxx&nickname=xxx   -> 불편
+    // 전송할 데이터를 JSON 객체로 묶어서 보내줘 내가 풀어서 쓸게
+    // @RequestBody
+    /*
+        {
+            "account": "xxx",
+            "password": "xxx",
+            "nickname": "xxx"
+        }
+     */
+    @PostMapping
+    public String join(@RequestBody Member member) {
+        member.setUid(UUID.randomUUID().toString());
+        memberStore.put(member.getUid(), member);
+        return "새로운 멤버 생성 - 닉네임: " + member.getNickname();
+    }
+
+    /*
+        콘솔창 이용하여 join api 테스트
+        fetch('http://localhost:9000/api/v3-2/members', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                account: 'power1234',
+                password: '9944',
+                nickname: '호랑이'
+            })
+        });
+
+        프로그램을 이용하여 api 테스트
+        Postman 다운로드
+     */
 }
