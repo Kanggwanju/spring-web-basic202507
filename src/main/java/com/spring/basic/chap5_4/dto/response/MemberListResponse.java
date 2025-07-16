929a5ca4d750c8ccfb3dd3bf9992b4b1014c23c1
Kanggwanju <xprtktm1@gmail.com>
2025-07-16 10:53:21 +0900

    회원 목록 응답 데이터 마스킹 및 MemberListResponse DTO 추가

---
+++ src/main/java/com/spring/basic/chap5_4/dto/response/MemberListResponse.java
@@ -0,0 +1,17 @@
+package com.spring.basic.chap5_4.dto.response;
+
+import lombok.*;
+
+// 클라이언트에게 멤버 목록을 보내줄 때 사용할 응답 DTO
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberListResponse {
+    private String id;
+    private String email; // account에 대응
+    private String nick;
+}
